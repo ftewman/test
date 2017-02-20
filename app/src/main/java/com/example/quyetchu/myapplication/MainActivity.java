e862c59f6d1a7f5e465653fa3f8b0cb490fa758f
QuyetChu <quyetchunguyen@gmail.com>
2017-02-20 11:52:13 +0700

    Revert "Update class student test int old"
    This reverts commit a655ee3e496aa4f06260e1e0b667aa29a1d95c37.

--- app/src/main/java/com/example/quyetchu/myapplication/MainActivity.java
+++ app/src/main/java/com/example/quyetchu/myapplication/MainActivity.java
@@ -2,7 +2,6 @@ package com.example.quyetchu.myapplication;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,10 +10,5 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toast.makeText(MainActivity.this, "Hi,Quyet", Toast.LENGTH_SHORT).show();
-
-    public void test(){
-        Log.i("MainActivity","log tét");
     }
-
 }
