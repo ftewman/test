d1453360d28e67c41a7781fab295be441566c92c
QuyetChu <quyetchunguyen@gmail.com>
2017-02-20 11:56:35 +0700

    update

--- app/src/main/java/com/example/quyetchu/myapplication/MainActivity.java
+++ app/src/main/java/com/example/quyetchu/myapplication/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.quyetchu.myapplication;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,6 +13,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Toast.makeText(MainActivity.this, "Hi,Quyet", Toast.LENGTH_SHORT).show();
+        test();
+
+    }
 
     public void test(){
         Log.i("MainActivity","log tét");
